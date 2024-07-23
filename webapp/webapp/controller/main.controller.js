7444aebdea2a95110c985f4f3ccc08a47ada3b9a
Siddharth Hota <siddharth.hota@incture.com>
2024-07-23 07:16:58 +0000

    Add all screen & side navigation css

--- webapp/webapp/controller/main.controller.js
+++ webapp/webapp/controller/main.controller.js
@@ -1,15 +1,19 @@
 sap.ui.define(['sap/ui/core/mvc/Controller',
 	"fpchopchop/webapp/model/formatter",
 	'sap/ui/model/json/JSONModel',
+	"sap/ui/model/Filter",
+	"sap/ui/model/FilterOperator",
 	"sap/viz/ui5/data/FlattenedDataset",
     "sap/viz/ui5/controls/common/feeds/FeedItem"
 ],
-	function (Controller, formatter,JSONModel, FlattenedDataset, FeedItem) {
+	function (Controller, formatter,JSONModel,Filter,FilterOperator, FlattenedDataset, FeedItem) {
 		"use strict";
 
 		return Controller.extend("fpchopchop.webapp.controller.main", {
 			formatter: formatter,
 			onInit: function () {
+				var oModel = this.getOwnerComponent().getModel("DeliveryOrder");
+				this._oGlobalFilter = null;
 				var data = [{
 					"Category": "Category 1",
 					"Revenue": 100
@@ -72,6 +76,31 @@ sap.ui.define(['sap/ui/core/mvc/Controller',
 				// Add the FeedItems to the VizFrame
 				oChartContainer.addFeed(oCategoryFeed);
 				oChartContainer.addFeed(oRevenueFeed);
-			}
+			},
+			_filter: function() {
+				if (this._oGlobalFilter) {
+					console.log(this._oGlobalFilter)
+					this.byId("DeliveryOrderTable").getBinding().filter(this._oGlobalFilter, "Application");
+				}
+			},
+	
+			filterGlobally: function(oEvent) {
+				const sQuery = oEvent.getParameter("query");
+				this._oGlobalFilter = null;
+				if (sQuery) {
+					this._oGlobalFilter = new Filter([
+						new Filter("Plant", FilterOperator.Contains, sQuery),
+						new Filter("Delivery", FilterOperator.Contains, sQuery),
+						new Filter("Article Code", FilterOperator.Contains, sQuery),
+						new Filter("Picker Name", FilterOperator.Contains, sQuery),
+						new Filter("Picked Qty", FilterOperator.Contains, sQuery),
+						new Filter("Picking Status", FilterOperator.Contains, sQuery),
+						new Filter("Start Time", FilterOperator.Contains, sQuery),
+						new Filter("End Time", FilterOperator.Contains, sQuery),
+						new Filter("Ship to Party", FilterOperator.Contains, sQuery)
+					], false);
+				}
+				this._filter();
+			},
 		});
 	});
